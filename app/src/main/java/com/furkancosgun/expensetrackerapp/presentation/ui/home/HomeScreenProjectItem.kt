11d7e2eaa1fc909bc09d6e9a494c7d99dc07d947
Furkan COSGUN <furkan51cosgun@gmail.com>
2024-01-14 18:53:56 +0300

    Navigation BUG FIX and manuel expense screen completed

---
+++ app/src/main/java/com/furkancosgun/expensetrackerapp/presentation/ui/home/HomeScreenProjectItem.kt
@@ -0,0 +1,99 @@
+package com.furkancosgun.expensetrackerapp.presentation.ui.home
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.furkancosgun.expensetrackerapp.R
+import com.furkancosgun.expensetrackerapp.domain.model.RecentExpenseReport
+import com.furkancosgun.expensetrackerapp.presentation.ui.common.AppTextButton
+import com.furkancosgun.expensetrackerapp.presentation.ui.common.UIPadding
+import com.furkancosgun.expensetrackerapp.presentation.ui.theme.ExpenseTrackerTheme
+import com.furkancosgun.expensetrackerapp.presentation.ui.theme.Yellow050
+import com.furkancosgun.expensetrackerapp.presentation.ui.theme.Yellow600
+
+@Composable
+fun HomeScreenProjectItem(modifier: Modifier = Modifier, expenseReport: RecentExpenseReport) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(UIPadding.MEDIUM.size),
+        colors = CardDefaults.cardColors(containerColor = Color.Unspecified),
+        border = BorderStroke(1.dp, Color.Gray),
+    ) {
+        Column(
+            modifier = Modifier.padding(UIPadding.MEDIUM.size),
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(UIPadding.MEDIUM.size),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = expenseReport.title, style = MaterialTheme.typography.titleMedium)
+                if (!expenseReport.isSubmitted) {
+                    Card(
+                        colors = CardDefaults.cardColors(
+                            containerColor = Yellow050
+                        )
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(UIPadding.SMALL.size),
+                            text = stringResource(
+                                R.string.not_submitted
+                            ),
+                            color = Yellow600,
+                            style = MaterialTheme.typography.titleSmall
+                        )
+                    }
+                }
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = expenseReport.toStringCurrencyWithAmount())
+                AppTextButton(text = "View More") {
+
+                }
+            }
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(UIPadding.MEDIUM.size)
+            ) {
+                Text(text = expenseReport.lastChangedDate)
+                Text(text = "|")
+                Text(text = stringResource(R.string.s_expense,expenseReport.totalExpenseCount))
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun HomeScreenProjectItem() {
+    val expenseReport = RecentExpenseReport(
+        title = "ARMADA FOODS",
+        totalExpenseCount = 21,
+        totalExpenseAmount = 100.12,
+        lastChangedDate = "01.01.2023",
+        isSubmitted = false, currency = "$"
+    )
+    ExpenseTrackerTheme {
+        HomeScreenProjectItem(
+            modifier = Modifier,
+            expenseReport = expenseReport
+        )
+    }
+}
